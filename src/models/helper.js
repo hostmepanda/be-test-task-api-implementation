1ac4d6e41f919912dd08942df2b2388b570521b8
hostmepanda <noah.vite@gmail.com>
2022-03-12 21:21:12 +0300

    Add todo to helper.js in models folder

--- src/models/helper.js
+++ src/models/helper.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 
+// TODO: move to store or database folder and rename respectively
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './database.sqlite3',
