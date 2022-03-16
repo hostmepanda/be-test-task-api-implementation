4af57114874442bf16afbecb43cecce69850d230
hostmepanda <noah.vite@gmail.com>
2022-03-16 09:23:54 +0300

    Add closing the app when can't connect to Redis

--- src/databases/redis.js
+++ src/databases/redis.js
@@ -3,7 +3,21 @@ const IoRedisClient = require('ioredis');
 const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_HOST || 6379;
 
-const singleRedisInstance = new IoRedisClient({ host: redisHost, port: redisPort });
+const singleRedisInstance = new IoRedisClient({
+  host: redisHost,
+  port: redisPort,
+});
+
+singleRedisInstance.on('error', (error) => {
+  if (error?.code === 'ECONNREFUSED') {
+    console.log(
+      'Closing the application\n',
+      'reason: Error happened while connecting to redis',
+      { details: error },
+    );
+    process.exit(1);
+  }
+});
 
 module.exports = {
   redis: singleRedisInstance,
